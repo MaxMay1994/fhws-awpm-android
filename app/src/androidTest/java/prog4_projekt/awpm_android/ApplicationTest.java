872b3c000fc8e9e962141d963b874217491a7cb8
Florian Duenow <florianduenow@Florians-MacBook-Air.local>
2016-04-06 13:39:08 +0200

    first commit II

---
+++ app/src/androidTest/java/prog4_projekt/awpm_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package prog4_projekt.awpm_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
